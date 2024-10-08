deea8ddd2af1a3de2c3b4cf740b14ca93925bcfb
fummbly <tstone449@gmail.com>
2024-10-08 16:05:54 -0400

    Created location getting

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	// Creating a pokeCLient for the config
-	pokeClient := pokeapi.NewClient(5 * time.Second)
+	pokeClient := pokeapi.NewClient(5*time.Second, 5*time.Minute)
 	cfg := &config{
 		client: pokeClient,
 	}
